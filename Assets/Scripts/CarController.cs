42ef5c66246aa36a980ba300bb6c4d7895ed23aa
Andrzej Zabski <jedrak32@gmail.com>
2020-10-27 23:39:08 +0100

    brezier curves

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,39 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PathCreation;
 
 public class CarController : MonoBehaviour
 {
     //[SerializeField] private bool _canMove = true;
     [SerializeField] private Rigidbody _rigidbody;
-    public float MaxSpeed = 1;
-    public float A = .001f;
+    public float MaxSpeed = 10;
+    public float A = .0001f;
     public float epsilon = .5f;
-    public Vector3 Direction = new Vector3(0.0f, 0.0f, 1.0f);
+    public PathCreator Path;
+    public float Speed = .0f;
+    private float distanceTravelled = 0.0f;
+
     private Coroutine accCorutine;
+
+
     public IEnumerator Accelarate(float a, float destSpeed)
     {
-        while(_rigidbody.velocity.magnitude < destSpeed)
+        while(Mathf.Abs(Speed) < destSpeed)
         {
-            _rigidbody.velocity += Direction * a;
-            Debug.Log(Direction * a);
+            Speed += a;
             yield return new WaitForEndOfFrame();
         }
         
     }
 
-    public IEnumerator Deaccelerate(float a, float destSpeed)
+    public IEnumerator Deaccelarate(float a, float destSpeed)
     {
-        
-
-        while (_rigidbody.velocity.magnitude > destSpeed)
+        while (Speed > destSpeed)
         {
-            Debug.Log(_rigidbody.velocity);
-            _rigidbody.velocity -= Direction * a;
-            if (_rigidbody.velocity.magnitude < epsilon) {
-                _rigidbody.velocity = Vector3.zero;
-                yield break;
-            }
+            Speed -= a;
             yield return new WaitForEndOfFrame();
         }
 
@@ -42,25 +40,23 @@ public class CarController : MonoBehaviour
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
-        
         accCorutine = StartCoroutine(Accelarate(A, MaxSpeed));
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        //Debug.Log(_rigidbody.velocity);
-        // _canMove = ? true : flase
+        distanceTravelled += Speed * Time.deltaTime;
+        transform.position = Path.path.GetPointAtDistance(distanceTravelled);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("brake"))
         {
-            StopCoroutine(accCorutine);
+            //StopCoroutine(accCorutine);
             Debug.Log("braking");
-            accCorutine =  StartCoroutine(Deaccelerate(.4f*A, 0));
+            //accCorutine =  StartCoroutine(Deaccelarate(-A, 0));
         }
     }
 }
