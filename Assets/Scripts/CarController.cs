3f8c5f86ab26aaed6836614d64d035f009527d7e
Andrzej Zabski <jedrak32@gmail.com>
2020-10-28 21:40:00 +0100

    car stopped before lights

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -6,15 +6,26 @@ using PathCreation;
 public class CarController : MonoBehaviour
 {
     //[SerializeField] private bool _canMove = true;
-    [SerializeField] private Rigidbody _rigidbody;
+    //
+
+    [Header("DRIVER PARAMETERS")]
+    public PathCreator Path;
+    public float Speed = 20.0f;
+    public float SeenDistance = 15;
+    [Header("CAR PARAMETERS")]
     public float MaxSpeed = 10;
     public float A = .0001f;
-    public float epsilon = .5f;
-    public PathCreator Path;
-    public float Speed = .0f;
-    private float distanceTravelled = 0.0f;
-
-    private Coroutine accCorutine;
+    [Header("LIGHT")]
+    [SerializeField] private LightController _observedLight = null;
+    [Header("RAYCASTING")]
+    //Things for raycasting
+    public float CheckingFrequency = 1;
+    public float Angle = 90;
+    public float Step = 1;
+    [Header("DEBUG")]
+    [SerializeField] private float distanceTravelled = 0.0f;
+    [SerializeField] private Coroutine accCorutine;
+    [SerializeField] private bool _braking = false;
 
 
     public IEnumerator Accelarate(float a, float destSpeed)
@@ -22,6 +33,7 @@ public class CarController : MonoBehaviour
         while(Mathf.Abs(Speed) < destSpeed)
         {
             Speed += a;
+            //Debug.Log("Accelarate");
             yield return new WaitForEndOfFrame();
         }
         
@@ -32,22 +44,84 @@ public class CarController : MonoBehaviour
         while (Speed > destSpeed)
         {
             Speed -= a;
+            //Debug.Log("Deaccelarate");
             yield return new WaitForEndOfFrame();
         }
+        _braking = false;
+    }
+
+    public IEnumerator CheckForObstacles()
+    {
+        while (true)
+        {
+            //Debug.Log(gameObject.name + " " + "Checking for obstacles...");
+            
+            for(Vector3 i =  Quaternion.Euler(0.0f, -Angle/2, 0.0f) * transform.forward;
+                i != Quaternion.Euler(0.0f, Angle / 2, 0.0f) * transform.forward;
+                i = Quaternion.Euler(0.0f, Step, 0.0f) * i)
+            {
+                for(Vector3 j = Quaternion.Euler(-Angle / 2, 0.0f, 0.0f)*i;
+                    j != Quaternion.Euler(Angle / 2, 0.0f, 0.0f) * i;
+                    j = Quaternion.Euler(Step, 0.0f, 0.0f) * j)
+                {
+                    Debug.DrawRay(transform.position, j * 10, Color.white, 1/CheckingFrequency);
+                    //Look for lights
+                    if(Physics.Raycast(transform.position, j, out RaycastHit hit, SeenDistance))
+                    {
+                        
+                        GameObject HitObject = hit.collider.gameObject;
+                        //Debug.Log("Seeng something... " + HitObject.name);
+                        LightController light = HitObject.GetComponent<LightController>();
+                        if (light != null)
+                        {
+                            //Debug.DrawRay(transform.position, j * 30, Color.red, 2);
+                            //Debug.Log(gameObject.name +" : I'm seeing Traffic Light Object: " + HitObject.name);
+                            if(!light._canGoThrought) BrakeBeforeLight(light);
+                        }
+                    }
+                }
+                
+            }
+            yield return new WaitForSeconds(1 / CheckingFrequency);
+        }
+    }
+
+    private void BrakeBeforeLight(LightController light)
+    {
+        _braking = true;
+        _observedLight = light;
+        //StopCoroutine(accCorutine);
+        accCorutine = StartCoroutine(Deaccelarate(A, 0));
 
     }
+
     // Start is called before the first frame update
     void Start()
     {
-        _rigidbody = GetComponent<Rigidbody>();
         accCorutine = StartCoroutine(Accelarate(A, MaxSpeed));
+        StartCoroutine(CheckForObstacles());
     }
 
     // Update is called once per frame
     void Update()
     {
+        //moving
         distanceTravelled += Speed * Time.deltaTime;
         transform.position = Path.path.GetPointAtDistance(distanceTravelled);
+        transform.rotation = Path.path.GetRotationAtDistance(distanceTravelled);
+        
+        //observe light
+        if(_observedLight != null)
+        {
+            if(_observedLight._canGoThrought)
+            {
+                _observedLight = null;
+                //StopCoroutine(accCorutine);
+                accCorutine = StartCoroutine(Accelarate(A, MaxSpeed));
+            }
+        }
+
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -55,8 +129,8 @@ public class CarController : MonoBehaviour
         if(other.gameObject.CompareTag("brake"))
         {
             //StopCoroutine(accCorutine);
-            Debug.Log("braking");
-            //accCorutine =  StartCoroutine(Deaccelarate(-A, 0));
+            //Debug.Log("braking");
+            //accCorutine =  StartCoroutine(Deaccelarate(A, 0));
         }
     }
 }
---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    [SerializeField] public bool _canGoThrought { get; private set; } = false;
+    public float ChangeInterval = 1; //seconds
+
+    private Material _currentMaterial;
+    private float _timer;
+
+    void Start()
+    {
+        _currentMaterial = gameObject.GetComponent<MeshRenderer>().material;
+        _currentMaterial.color = Color.green;
+        _canGoThrought = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timer += Time.deltaTime;
+        //Debug.Log(_canGoThrought);
+        if(_timer > ChangeInterval)
+        {
+            _timer = 0;
+            _canGoThrought = !_canGoThrought;
+            if (_canGoThrought)
+            {
+                 _currentMaterial.color = Color.green;
+            }
+            else
+            {
+                _currentMaterial.color = Color.red;
+            }
+        }
+    }
+}
