fdbb372a0f1ef79f69a71a9ff069f5062bf3e17d
Andrzej Zabski <jedrak32@gmail.com>
2020-12-13 16:11:32 +0100

    CarMaterial manager script

---
+++ Assets/Scripts/CarMaterialManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMaterialManager : MonoBehaviour
+{
+    public Material material;
+    [SerializeField] private Car car;
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+        car = GetComponentInParent<Car>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        material.color = Vector4.Lerp(Color.red, Color.green, car.Speed / car.MaxSpeed);
+    }
+}
